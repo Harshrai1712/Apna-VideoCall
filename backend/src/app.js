c699f89cddf57f9a822dd1b571990b8dea5b4a29
Harshrai1712 <harshrai1217@gmail.com>
2026-01-20 18:30:40 +0530

    update the database string

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+    const connectionDb = await mongoose.connect("mongodb+srv://harshrai17:Harsh123@cluster0.7nwbiog.mongodb.net/Apna_VideoCal?appName=harshrai&retryWrites=true&w=majority");
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
